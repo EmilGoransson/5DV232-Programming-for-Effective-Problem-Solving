aa351e869b751a6957a2e5fa217633d3afc3d9af
EmilGoransson <emilgo@kth.se>
2023-07-12 14:54:13 +0200

    shortened

--- week 4+/simplearithmetic.kt
+++ week 4+/simplearithmetic.kt
@@ -1,9 +1,6 @@
 import java.math.BigDecimal
 
 fun main() {
-    val (a, b, c) = readLine()!!.split(' ').map(String::toInt)
-    val A = BigDecimal(a).setScale(18)
-    val B = BigDecimal(b).setScale(18)
-    val C = BigDecimal(c).setScale(18)
-    println(A * B / C)
+    val (a, b, c) = readLine()!!.split(' ').map { i -> BigDecimal(i.toInt()).setScale(9) }
+    println(a * b / c)
 }
